19747df04562ba4d8dc1642d0289cc42c58cabd4
Joseph Langford <misterguruman@Josephs-MacBook-Air.local>
2023-12-17 12:40:48 -0500

    loop, and while shenangans

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,29 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let delay = time::Duration::from_secs(1);
+    let mut plz_no_infinite_loops: i32 = 5;
+
+    loop {
+        if plz_no_infinite_loops < 0 {
+            break;
+        }
+
+        println!("Counting down to liftoff: {}", plz_no_infinite_loops);
+        plz_no_infinite_loops = plz_no_infinite_loops - 1;
+        thread::sleep(delay);
+    };
+
+    println!("You have reached liftoff!");
+    plz_no_infinite_loops = 0;
+
+    while plz_no_infinite_loops <= 5 {
+        let altitude_offset = plz_no_infinite_loops * 10_000;
+
+        println!("Your current altitude is {}", altitude_offset);
+        plz_no_infinite_loops = plz_no_infinite_loops + 1;
+        thread::sleep(delay);
+    };
+
+    println!("You made it to space!")
 }
